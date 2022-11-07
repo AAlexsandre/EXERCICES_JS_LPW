d34c73083b5f38207a302358b943a89a60c2b0f6
efp2007 <efp2007@form.efp.local>
2022-11-07 11:07:52 +0100

    separate folders for CSS and JS, is good practice, the one for the html is weird. remove it

--- to_do_list/js/to_do_list.js
+++ to_do_list/js/to_do_list.js
@@ -10,11 +10,11 @@ function add_to_the_list(event) {
 
     //to create the elemnts
     //1
-    let add_element = document.createElement("li");
-    let test = document.createElement("span");
+    let add_element_li = document.createElement("li");
+    let add_element_span = document.createElement("span");
 
     //2
-    test.innerText = input_text.value;
+    add_element_span.innerText = input_text.value;
 
     //3
     let delete_button = document.createElement("button");
@@ -28,28 +28,28 @@ function add_to_the_list(event) {
 
     delete_button.innerText = "Delete";
     delete_button.addEventListener("click", function () {
-        to_do_list.removeChild(add_element);
+        to_do_list.removeChild(add_element_li);
     });
 
     modify_button.innerText = "Modify?";
     done_button.innerText = "Done";
 
     done_button.addEventListener("click", function () {
-        add_element.style.textDecoration = "line-through";
-        to_do_list.appendChild(add_element);
+        add_element_li.style.textDecoration = "line-through";
+        to_do_list.appendChild(add_element_li);
         done_button.innerText = "Not done";
     });
 
     done_button.addEventListener("dblclick", function () {
-        add_element.style.textDecoration = "none";
+        add_element_li.style.textDecoration = "none";
         done_button.innerText = "done";
     });
 
-    add_element.appendChild(test);
-    add_element.appendChild(delete_button);
-    add_element.appendChild(modify_button);
-    add_element.appendChild(done_button);
-    to_do_list.appendChild(add_element);
+    add_element_li.appendChild(add_element_span);
+    add_element_li.appendChild(delete_button);
+    add_element_li.appendChild(modify_button);
+    add_element_li.appendChild(done_button);
+    to_do_list.appendChild(add_element_li);
 
     clear_the_input();
 }

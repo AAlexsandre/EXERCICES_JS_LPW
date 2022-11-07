59d48bf798465940d444857ab4f0ee9e191e54ca
efp2007 <efp2007@form.efp.local>
2022-11-07 11:35:44 +0100

    write function colorize(text, class)

--- to_color_the_sentences/js/colorizer.js
+++ to_color_the_sentences/js/colorizer.js
@@ -1,38 +1,59 @@
 let button = document.getElementById("button_colorize").addEventListener("click", create_the_sentences_color);
+let colorised_id = document.getElementById("colorized");
 
 /**
  * This function creates the span elements and adds a caracter inside wich them
  */
 function create_the_sentences_color(event) {
-    let colorized_id = document.getElementById("colorized")
     event.preventDefault();
-    let remove_the_elements = colorized_id;
-    cleanElement(remove_the_elements);  
+    let remove_the_elements = colorised_id;
+    cleanElement(remove_the_elements);
 
     let input_value = document.getElementById("text").value;
-
-    let new_span;
-    let new_lettre;
-    //take the id about the div
-    let current_div = colorized_id;
-    let current_colour;
     let colour_index = 0;
 
-    for (let i = 0; i < input_value.length; ++i) {
+    colorise(input_value, colour_index);
+}
+
+
+/**
+ * This function cleans the all elements
+ * @param {String} remove_the_elements 
+ */
+function cleanElement(remove_the_elements) {
+    while (remove_the_elements.firstChild) {
+        remove_the_elements.removeChild(remove_the_elements.firstChild);
+    }
+}
+
+
+/**
+ * This function add the elements with the colors
+ * @param {String} text 
+ * @param {number} color 
+ */
+function colorise(text, color) {
+    let new_span;
+    let new_lettre;
+
+    //take the id about the div
+    let current_div = colorised_id;
+    let current_colour;
+    for (let i = 0; i < text.length; ++i) {
 
         //create the element span
         new_span = document.createElement("span");
 
-        if (input_value.charAt(i) != " ") {
-            current_colour = "color_" + (colour_index % 3 + 1);
+        if (text.charAt(i) != " ") {
+            current_colour = "color_" + (color % 3 + 1);
 
             //add the class
             new_span.classList.add(current_colour);
-            ++colour_index;
+            ++color;
         }
 
         //take the current lettre about the word
-        new_lettre = document.createTextNode(input_value.charAt(i));
+        new_lettre = document.createTextNode(text.charAt(i));
 
         //add the lettre inside the span
         new_span.appendChild(new_lettre);
@@ -40,12 +61,5 @@ function create_the_sentences_color(event) {
         //add the span inside of div
         current_div.appendChild(new_span);
 
-    }
-}
-
-
-function cleanElement(remove_the_elements){
-    while (remove_the_elements.firstChild) {
-        remove_the_elements.removeChild(remove_the_elements.firstChild);
     }
 }

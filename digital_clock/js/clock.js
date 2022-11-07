67e9c0f3fb35a15acb7df3a6299d55ba38163d32
efp2007 <efp2007@form.efp.local>
2022-11-07 12:23:29 +0100

    2.3.5 JS Avancé • utiliser la notation arrow function pour vos fonctions • écrire la fonction startClock sur 3 lignes

--- digital_clock/js/clock.js
+++ digital_clock/js/clock.js
@@ -1,16 +1,17 @@
 /**
+ * This function add a leading zero to a digit
+ * @param {number} digit 
+ */
+// function leadingZeros(digit) { return (digit < 10) ? "0" + digit : digit; }
+
+const leadingZeros = (digit) => { return (digit < 10) ? "0" + digit : digit; }
+
+/**
  * This function create the digital clock
  */
 function startClock() {
     let clock = new Date;
-    let tabOfClock = [clock.getHours(), clock.getMinutes(), clock.getSeconds()].map(leadingZeros);
-    document.getElementById("clock").textContent = tabOfClock.join(":");
+    document.getElementById("clock").textContent = [clock.getHours(), clock.getMinutes(), clock.getSeconds()].map(leadingZeros).join(":");
     setTimeout(startClock, 1000);
 }
 startClock();
-
-/**
- * This function add a leading zero to a digit
- * @param {number} digit 
- */
-function leadingZeros(digit) { return (digit < 10) ? "0" + digit : digit; } 

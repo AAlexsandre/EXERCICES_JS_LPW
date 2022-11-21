f3b2e4bf3f96265f1a8d39cf8efbde4a3394064a
efp2007 <efp2007@form.efp.local>
2022-11-21 15:59:01 +0100

    i and j change name to row and col

--- Damier/js/damier.js
+++ Damier/js/damier.js
@@ -1,11 +1,8 @@
 // 1 : Identify the data in the url
-const urlParams = window.location.href.split('?')[1];
-const queryString = new URLSearchParams(urlParams);
-window.history.replaceState({}, '', `${location.pathname}?L=&W=`);
-
 // 2 : Give the "boardLength" and "boardWidth" the data
 let boardLength;
 let boardWidth;
+let cell;
 
 // 3 : Determinate the pieces and the colors
 const tabColor = ["white", "black"];
@@ -14,98 +11,82 @@ let indexColor = 0;
 // ["WHITE_ROOK", "♖"], ["WHITE_KNIGHT", "♘"], ["WHITE_BISHOP", "♗"], ["WHITE_KING", "♔"], ["WHITE_QUEEN", "♕"] 
 // ["BLACK_ROOK", "♜"], ["BLACK_KNIGHT", "♞"], ["BLACK_BISHOP", "♝"], ["BLACK_KING", "♚"], ["BLACK_QUEEN", "♛"]
 
-const whitePositions = new Array (["♖"],["♘"],["♗"],["♔"],["♕"],["♗"],["♘"],["♖"],["♙"]);
-const blackPositions = new Array (["♜"],["♞"],["♝"],["♚"],["♛"],["♝"],["♞"],["♜"],["♟"]);
-
-console.log(whitePositions);
-console.log(blackPositions);
+const whitePositions = ["♖","♘","♗","♔","♕","♗","♘","♖","♙"];
+const blackPositions = ["♜","♞","♝","♚","♛","♝","♞","♜","♟"];
 
 // 4 : Create the board
-const board = document.createElement("div");
-board.setAttribute('id', 'board');
-document.body.appendChild(board);
 
-fetchData();
 
+let res = dimensionsFromURLParams();
+boardLength = res[0];
+boardWidth = res[1];
+
+createBoard(boardLength, boardWidth);
 // 5 : Check if the parameters are defined
-let cell;
-
-if (boardLength === '') {
-    boardLength = 8;
-}
-if (boardWidth === '') {
-    boardWidth = 8;
-}
-
-// 6 : Create the cells on the board
-for (let i = 0; i < boardLength; ++i) {
-    for (let j = 0; j < boardWidth; ++j) {
-        if (boardLength != 8 || boardWidth != 8) {
-            no_chessboard(i, j);
-        } else {
-            chessboard(i, j);
-        }
-
-    }
-}
 
 /**
  * This function fetch the data
  */
-function fetchData() {
-    for (let pair of queryString.entries()) { // Object.entries() renvoie un tableau des propriétés sous la forme de paires [clé, valeur]
-        if (pair[0] === "L") {
-            boardLength = pair[1];
+function dimensionsFromURLParams() {
+    const queryString = new URLSearchParams(document.location.search);
+    let length = parseInt(queryString.get("L"), 10);
+    let width = parseInt(queryString.get("W"), 10);
 
-        } else if (pair[0] === "W") {
-            boardWidth = pair[1];
-        }
+    if (isNaN(length) || length <= 0) {
+        length = 8;
     }
+    if (isNaN(width) || width <= 0) {
+        width = 8;
+    }
+    return [length, width];
+}
+
+
+function createBoard(boardLength, boardWidth)
+{
+    const isChessboard = boardLength == 8 && boardWidth == 8;
+    const board = document.createElement("div");
+
+    board.setAttribute('id', 'board');
+    document.body.appendChild(board);
+    
     board.style.gridTemplateColumns = `repeat(${boardWidth}, 1fr)`;
     board.style.gridTemplateRows = `repeat(${boardLength}, 1fr)`;
-}
+    
+    
+    // 6 : Create the cells on the board
+    for (let row = 0; row < boardLength; ++row) {
+        for (let col = 0; col < boardWidth; ++col) {
 
-/**
- * This function calls the other function to create the cells on the board
- * TODO: quelle autre fonction?
- * @param {number} i 
- * @param {number} j 
- */
-function no_chessboard(i, j) {
-    createCell(i, j);
-}
-
-/**
- * This function calls the other function to create the cells and the pieces on the board 
- * TODO: quelle autre fonction?
- * @param {number} i 
- * @param {number} j 
- */
-function chessboard(i, j) {
-    createCell(i, j);
-
-    if (i === 0) {
-        cell.innerText = whitePositions[j];
-    }
-    else if (i === 1) {
-        cell.textContent = whitePositions[8];
-
-    } else if (i === 6) {
-        cell.innerText = blackPositions[8];
-
-    } else if (i === boardLength - 1) {
-        console.log(j);
-        cell.innerText = blackPositions[j];
+            board.appendChild(createCell(row,col,isChessboard));
+        }
     }
 }
 
 /**
  * This fonction creates the cells on the board
- * @param {number} i 
- * @param {number} j 
+ * @param {number} row 
+ * @param {number} col 
  */
-function createCell(i, j) {
-    cell = document.createElement("div");
-    cell.setAttribute('class', tabColor[(i + j) % 2]);
-    board.appendChild(cell);
+function createCell(row, col, isChess) {
+    let cell = document.createElement("div");
+    cell.setAttribute('class', tabColor[(row + col) % 2]);
+
+    if(isChess){
+        if (row === 0) {
+            cell.innerText = whitePositions[col];
+        }
+        else if (row === 1) {
+            cell.textContent = whitePositions[8];
+    
+        } else if (row === 6) {
+            cell.innerText = blackPositions[8];
+    
+        } else if (row === boardLength - 1) {
+            console.log(col);
+            cell.innerText = blackPositions[col];
+        }
+    }
+
+    return cell;
 }

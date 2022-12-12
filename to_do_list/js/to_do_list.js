7331409899bcd6145a417849596e3c016c0baa99
efp2007 <efp2007@form.efp.local>
2022-12-12 16:43:16 +0100

    to do list need to modify done

--- to_do_list/js/to_do_list.js
+++ to_do_list/js/to_do_list.js
@@ -1,14 +1,95 @@
-let addNewLi = document.getElementById("button").addEventListener("click", createTheLine);
-let increment = 0;
-let test = [];
+let button = document.querySelector("#newTask button");
+let tabKeysFromStorage = new Array();
+let tabValuesFromStorage = new Array();
 
-loadLocalStorageTask();
+button.addEventListener("click", function (event) {
+    event.preventDefault();
 
-function loadLocalStorageTask() {
+    let lastKey = document.getElementById('lastKey').value;
+
+    if (lastKey == "") {
+        lastKey = 0;
+    }
+
+    document.getElementById('lastKey').value = ++lastKey;
+
+
+    let taskName = document.getElementById("text").value;
+
+    let listId = "listNotDone";
+
+    addTaskToList(taskName, listId, lastKey);
+    addTaskToStorage(taskName, listId, lastKey);
+
+
+    // clear The Input
+    document.getElementById("text").value = "";
+});
+
+let lastKey = displayTasksFromStorage();
+document.getElementById('lastKey').value = lastKey;
+
+function displayTasksFromStorage() {
+    if (localStorage.length == 0) {
+        return 0;
+    }
+
+    let lastKey;
+
     for (let i = 0; i < localStorage.length; ++i) {
-        createElements(i);
+        lastKey = localStorage.key(i);
+        tabKeysFromStorage[i] = localStorage.key(i);
+        tabValuesFromStorage[i] = localStorage.getItem(tabKeysFromStorage[i]);
+    }
+    tabKeysFromStorage.sort()
+    console.log(tabValuesFromStorage);
+
+    for (let j = 0; j < tabKeysFromStorage.length; ++j) {
+        if(tabValuesFromStorage[j].charAt(tabValuesFromStorage[j].length-1) == "#"){
+            addTaskToList(localStorage.getItem(tabKeysFromStorage[j]), 'listDone', j);
+        } else {
+            addTaskToList(localStorage.getItem(tabKeysFromStorage[j]), 'listNotDone', j);
+        }
+    }
+
+    return lastKey;
+}
+
+function addTaskToList(taskName, listId, lastKey) {
+
+    // add task to UI
+    let newLi = document.createElement("li");
+    newLi.setAttribute("id", lastKey);
+
+    let newSpan = document.createElement("span");
+    newSpan.innerText = taskName;
+
+    newLi.appendChild(newSpan);
+    newLi.appendChild(createActionableDelete());
+    newLi.appendChild(createActionableModify());
+    newLi.appendChild(createActionableDone());
+
+    list = document.getElementById(listId);
+    list.appendChild(newLi);
+}
+
+function addTaskToStorage(taskName, listId, lastKey) {
+    localStorage.setItem(lastKey, taskName);
+}
+
+function removeTaskFromList(e) {
+    e.target.parentNode.remove();
+}
+
+function removeTaskFromStorage(e) {
+    let taskId = e.target.parentNode.getAttribute("id");
+    localStorage.removeItem(taskId);
+
+    if (document.getElementById("listNotDone").childNodes.length == 0) {
+        localStorage.clear();
     }
 }
+
 /**
  * @return deleteButton;
  */
@@ -18,37 +99,37 @@ function createActionableDelete() {
     deleteButton.innerText = "Delete";
 
     deleteButton.addEventListener("click", function (e) {
-        localStorage.removeItem(e.target.parentNode.id);
-        e.target.parentNode.remove();
-        
+        removeTaskFromList(e);
+        removeTaskFromStorage(e);
+
     });
 
     return deleteButton;
 }
 
+
 /**
  * @return modifyButton;
  */
 function createActionableModify() {
-    let modifyButton = document.createElement("button");
+    modifyButton = document.createElement("button");
     modifyButton.setAttribute("class", "modify");
     modifyButton.innerText = "Modify";
 
     modifyButton.addEventListener("click", function (e) {
 
-        //mettre la donnée dans l'input
         loadEditForm(this.parentElement.getElementsByTagName("span")[0]);
 
-        //afficher le formulaire d'édition
-        show('editForm');
+        let form = document.getElementById("editForm");
+
+        toggle(form);
 
         //Ajouter un actionListener sur le boutton editSubmit
         let submit = document.getElementById('editSubmit');
         submit.addEventListener('click', function (e) {
             e.preventDefault();
-            updateTaskContent();
-            hide('editForm');
-        });
+            modifyContent();
+        }, { once: true });
     });
 
     return modifyButton;
@@ -59,12 +140,12 @@ function createActionableModify() {
  */
 function createActionableDone() {
     let doneButton = document.createElement("button");
-    doneButton.setAttribute("class", "done");
     doneButton.innerText = "Done";
 
     doneButton.addEventListener("click", function () {
-        this.parentElement.style.textDecoration = "line-through";
         document.getElementById("listDone").appendChild(this.parentElement);
+        tabValuesFromStorage[this.parentElement.id] = tabValuesFromStorage[this.parentElement.id]+"#";
+        localStorage.setItem(tabKeysFromStorage[this.parentElement.id], tabValuesFromStorage[this.parentElement.id]+"#");
     });
 
     return doneButton;
@@ -76,35 +157,29 @@ function loadEditForm(task) {
     document.getElementById("editId").value = task.getAttribute('id');
 }
 
+function modifyContent() {
+    let content = document.getElementById("editContent").value;
 
-function show(id) {
-    let form = document.getElementById(id);
-    form.style.display = "flex";
-}
-
-function hide(id) {
-    let form = document.getElementById(id);
-    form.style.display = "none";
-}
-
-
-function updateTaskContent() {
-    let content = document.getElementById('editContent').value;
     let id = document.getElementById("editId").value;
+
     // assigner l'input
     document.getElementById(id).textContent = content;
+
+    document.getElementById("editSubmit").addEventListener("click", function () {
+        toggle(document.getElementById("editForm"));
+    });
 }
 
-function createTheLine(event) {
+function appendToTheList(event) {
     event.preventDefault();
 
     let taskContent = document.getElementById("text").value;
     // add task to UI
     let newLi = document.createElement("li");
-    newLi.setAttribute("id", increment);
+    newLi.setAttribute("id", lastKey);
 
     let newSpan = document.createElement("span");
-    newSpan.setAttribute("id", "elementNumber" + increment);
+    newSpan.setAttribute("id", "elementNumber" + lastKey);
     newSpan.innerText = taskContent;
 
     newLi.appendChild(newSpan);
@@ -117,25 +192,14 @@ function createTheLine(event) {
 
     // clear The Input
     document.getElementById("text").value = "";
-
-    // add task to storage
-    localStorage.setItem(increment, taskContent);
-
-    ++increment;
+    ++lastKey;
 }
 
-function createElements(i){
-    let createLi = document.createElement("li");
-    let createSpan = document.createElement("span");
+function toggle(form) {
+    if (form.style.display == "flex") {
+        form.style.display = "none";
 
-    createLi.appendChild(createSpan);
-    createSpan.textContent = localStorage.getItem(i);
-    createSpan.setAttribute("id", "elementNumber" + increment);
-    createLi.setAttribute("id", increment);
-
-    createLi.appendChild(createActionableDelete());
-    createLi.appendChild(createActionableModify());
-    createLi.appendChild(createActionableDone());
-
-    document.getElementById("listNotDone").appendChild(createLi);
+    } else {
+        form.style.display = "flex";
+    }
 }

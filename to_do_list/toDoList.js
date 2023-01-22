33a0f232df8faaf575a93e938dd5af51d9cc013b
Alexsandre Akofovi <52098@etu.he2b.be>
2023-01-22 23:31:06 +0100

    mise à jour css, javascript ajout du localStorage pas encore pour les tâches fait

--- to_do_list/toDoList.js
+++ to_do_list/toDoList.js
@@ -1,4 +1,5 @@
 let button = document.querySelector("button");
+// let inc = 0;
 
 button.addEventListener("click", function (event) {
     event.preventDefault();
@@ -13,31 +14,48 @@ let taskCounter = 0;
 let currentTask;
 
 function addNewTask(taskValue) {
+    if (typeof (Storage) !== "undefined") {
+        if (localStorage.getItem("tasks") === null) {
+            localStorage.setItem("tasks", JSON.stringify([]));
+        }
+        let tasks = JSON.parse(localStorage.getItem("tasks"));
+        tasks.push(taskValue);
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+    }
+
     let newTask = document.createElement("li");
     newTask.setAttribute("id", "task-" + taskCounter);
     taskCounter++;
-    newTask.innerHTML = taskValue;
-
+    addTaskName(newTask, taskValue);
     addButtons(newTask);
-
     document.getElementById("tasks").appendChild(newTask);
+}
+
+
+function addTaskName(newTask, taskValue) {
+    let div = document.createElement("div");
+    div.innerHTML = taskValue;
+    newTask.appendChild(div);
 }
 
 function addButtons(newTask) {
     let deleteButton = document.createElement("button");
-    deleteButton.classList.add("btn");
-    deleteButton.classList.add("btn-danger");
-    deleteButton.innerHTML = "Delete";
+    deleteButton.innerHTML = "DELETE";
     newTask.appendChild(deleteButton);
 
     deleteButton.addEventListener("click", function (e) {
+        if (typeof (Storage) !== "undefined") {
+            let tasks = JSON.parse(localStorage.getItem("tasks"));
+            let taskValue = this.parentElement.firstChild.textContent;
+            let index = tasks.indexOf(taskValue);
+            tasks.splice(index, 1);
+            localStorage.setItem("tasks", JSON.stringify(tasks));
+        }
         e.target.parentNode.remove();
     });
 
     let modifyButton = document.createElement("button");
-    modifyButton.classList.add("btn");
-    modifyButton.classList.add("btn-warning");
-    modifyButton.innerHTML = "Modify";
+    modifyButton.innerHTML = "MODIFY";
     modifyButton.setAttribute("data-task", "task-" + taskCounter);
     newTask.appendChild(modifyButton);
 
@@ -52,12 +70,16 @@ function addButtons(newTask) {
 
     document.getElementById("save-modify").addEventListener("click", function () {
         let task = document.getElementById("task-modify").value;
-        // let taskId = document.getElementById("modal-content").previousElementSibling.getAttribute("data-task");
-        console.log(document.getElementById("modal-content").parentElement.parentElement);
+        let tasks = JSON.parse(localStorage.getItem("tasks"));
+        let taskValue = currentTask.textContent;
+        let index = tasks.indexOf(taskValue);
+        tasks[index] = task;
+        localStorage.setItem("tasks", JSON.stringify(tasks));
         currentTask.textContent = task;
         toggle(modal);
     });
-    
+
+
 
     document.getElementById("close-modal").addEventListener("click", function () {
         toggle(modal);
@@ -66,14 +88,27 @@ function addButtons(newTask) {
 
 
     let doneButton = document.createElement("button");
-    doneButton.classList.add("btn");
-    doneButton.classList.add("btn-success");
-    doneButton.innerHTML = "Done";
+    doneButton.innerHTML = "DONE";
     newTask.appendChild(doneButton);
 
     doneButton.addEventListener("click", function () {
-        this.parentElement.classList.add("strike");
+
         document.getElementById("tasksDone").appendChild(this.parentElement);
+        this.parentElement.children[2].remove();
+        this.parentElement.removeChild(this);
+        // let taskDone = document.getElementById("tasksDone").children[inc].childNodes[0].innerHTML;
+
+        // if (typeof (Storage) !== "undefined") {
+        //     if (localStorage.getItem("tasksDone") === null) {
+        //         localStorage.setItem("tasksDone", JSON.stringify([]));
+        //     }
+        //     let tasks = JSON.parse(localStorage.getItem("tasksDone"));
+        //     tasks.push(taskDone);
+        //     localStorage.setItem("tasksDone", JSON.stringify(tasks));
+        // }
+
+        // ++inc;
+
     });
 
 }
@@ -83,5 +118,27 @@ function toggle(element) {
         element.style.display = "block";
     } else {
         element.style.display = "none";
+    }
+}
+
+
+window.addEventListener("load", function () {
+    displayTasks();
+});
+
+function displayTasks() {
+    if (typeof (Storage) !== "undefined") {
+        if (localStorage.getItem("tasks") !== null) {
+            let tasks = JSON.parse(localStorage.getItem("tasks"));
+            for (let i = 0; i < tasks.length; i++) {
+                let taskValue = tasks[i];
+                let newTask = document.createElement("li");
+                newTask.setAttribute("id", "task-" + taskCounter);
+                taskCounter++;
+                addTaskName(newTask, taskValue);
+                addButtons(newTask);
+                document.getElementById("tasks").appendChild(newTask);
+            }
+        }
     }
 }

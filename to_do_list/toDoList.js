77f6f22babf778bef1302adcad0062103acd12e6
Alexsandre Akofovi <52098@etu.he2b.be>
2023-01-28 18:00:02 +0100

    done refractory need code and check modal

--- to_do_list/toDoList.js
+++ to_do_list/toDoList.js
@@ -1,5 +1,4 @@
 let button = document.querySelector("button");
-// let inc = 0;
 
 button.addEventListener("click", function (event) {
     event.preventDefault();
@@ -14,6 +13,11 @@ let taskCounter = 0;
 let currentTask;
 
 function addNewTask(taskValue) {
+    checkInStorage(taskValue);
+    createTask(taskValue);
+}
+
+function checkInStorage(taskValue){
     if (typeof (Storage) !== "undefined") {
         if (localStorage.getItem("tasks") === null) {
             localStorage.setItem("tasks", JSON.stringify([]));
@@ -22,7 +26,9 @@ function addNewTask(taskValue) {
         tasks.push(taskValue);
         localStorage.setItem("tasks", JSON.stringify(tasks));
     }
+}
 
+function createTask(taskValue){
     let newTask = document.createElement("li");
     newTask.setAttribute("id", "task-" + taskCounter);
     taskCounter++;
@@ -31,36 +37,77 @@ function addNewTask(taskValue) {
     document.getElementById("tasks").appendChild(newTask);
 }
 
-
 function addTaskName(newTask, taskValue) {
     let div = document.createElement("div");
-    div.innerHTML = taskValue;
+
+    let pin = document.createElement("img");
+    pin.src = "6a2448a6.png"
+    newTask.appendChild(pin);
+    
+    let span = document.createElement("span");
+
+    let pin2 = document.createElement("img");
+    pin2.src = "6a2448a6.png"
+    newTask.appendChild(pin2);
+
+    span.innerHTML = taskValue;
+    div.appendChild(pin);
+    div.appendChild(span);
+    div.appendChild(pin2);
     newTask.appendChild(div);
 }
 
 function addButtons(newTask) {
+    addDelete(newTask);
+    addModify(newTask);
+    addDone(newTask);
+}
+
+function addDelete(newTask){
     let deleteButton = document.createElement("button");
     deleteButton.innerHTML = "DELETE";
     newTask.appendChild(deleteButton);
 
-    deleteButton.addEventListener("click", function (e) {
-        if (typeof (Storage) !== "undefined") {
-            let tasks = JSON.parse(localStorage.getItem("tasks"));
-            let taskValue = this.parentElement.firstChild.textContent;
-            let index = tasks.indexOf(taskValue);
-            tasks.splice(index, 1);
-            localStorage.setItem("tasks", JSON.stringify(tasks));
-        }
-        e.target.parentNode.remove();
-    });
+    erase(deleteButton);
+}
 
+function erase(deleteButton){
+    deleteButton.addEventListener("click", function (e) {
+        if(this.parentElement.parentElement.id == "tasks"){
+            if (typeof (Storage) !== "undefined") {
+                let tasks = JSON.parse(localStorage.getItem("tasks"));
+                let taskValue = this.parentElement.firstChild.textContent;
+                let index = tasks.indexOf(taskValue);
+                tasks.splice(index, 1);
+                localStorage.setItem("tasks", JSON.stringify(tasks));
+            }
+            e.target.parentNode.remove();
+        } else if (this.parentElement.parentElement.id == "tasksDone"){
+            if (typeof (Storage) !== "undefined") {
+                let todos = JSON.parse(localStorage.getItem("todos"));
+                let taskValue = this.parentElement.firstChild.textContent;
+                let index = todos.indexOf(taskValue);
+                todos.splice(index, 1);
+                localStorage.setItem("todos", JSON.stringify(todos));
+            }
+            e.target.parentNode.remove();
+        }
+
+    });
+}
+
+function addModify(newTask){
     let modifyButton = document.createElement("button");
     modifyButton.innerHTML = "MODIFY";
     modifyButton.setAttribute("data-task", "task-" + taskCounter);
     newTask.appendChild(modifyButton);
 
+    changeContent(modifyButton);
+}
+
+function changeContent(modifyButton){
+    let modal = document.getElementById("modal");
     modifyButton.addEventListener("click", function () {
-        let modal = document.getElementById("modal");
         let inputModify = document.getElementById("task-modify");
         let task = this.parentElement.firstChild.textContent;
         inputModify.value = task;
@@ -79,38 +126,44 @@ function addButtons(newTask) {
         toggle(modal);
     });
 
-
-
     document.getElementById("close-modal").addEventListener("click", function () {
         toggle(modal);
     });
+}
 
-
-
+function addDone(newTask){
     let doneButton = document.createElement("button");
     doneButton.innerHTML = "DONE";
     newTask.appendChild(doneButton);
 
-    doneButton.addEventListener("click", function () {
+    done(doneButton);
+}
 
+let todos = JSON.parse(localStorage.getItem("todos")) || [];
+
+function done(doneButton){
+    doneButton.addEventListener("click", function () {
+        let done = this.parentElement.firstChild.children[1].innerHTML;
         document.getElementById("tasksDone").appendChild(this.parentElement);
         this.parentElement.children[2].remove();
-        this.parentElement.removeChild(this);
-        // let taskDone = document.getElementById("tasksDone").children[inc].childNodes[0].innerHTML;
+        this.style.display = 'none';
 
-        // if (typeof (Storage) !== "undefined") {
-        //     if (localStorage.getItem("tasksDone") === null) {
-        //         localStorage.setItem("tasksDone", JSON.stringify([]));
-        //     }
-        //     let tasks = JSON.parse(localStorage.getItem("tasksDone"));
-        //     tasks.push(taskDone);
-        //     localStorage.setItem("tasksDone", JSON.stringify(tasks));
-        // }
+        if (typeof (Storage) !== "undefined") {
+            if (localStorage.getItem("todos") === null) {
+                localStorage.setItem("todos", JSON.stringify([]));
+            }
+            let todos = JSON.parse(localStorage.getItem("todos"));
+            todos.push(done);
+            localStorage.setItem("todos", JSON.stringify(todos));
 
-        // ++inc;
+            let tasks = JSON.parse(localStorage.getItem("tasks"));
+            let taskValue = this.parentElement.firstChild.textContent;
+            let index = tasks.indexOf(taskValue);
+            tasks.splice(index, 1);
+            localStorage.setItem("tasks", JSON.stringify(tasks));
+        }
 
     });
-
 }
 
 function toggle(element) {
@@ -122,11 +175,11 @@ function toggle(element) {
 }
 
 
-window.addEventListener("load", function () {
-    displayTasks();
-});
+window.addEventListener("load", displayTasks);
 
 function displayTasks() {
+    let taskValue;
+    let newTask;
     if (typeof (Storage) !== "undefined") {
         if (localStorage.getItem("tasks") !== null) {
             let tasks = JSON.parse(localStorage.getItem("tasks"));
@@ -137,7 +190,21 @@ function displayTasks() {
                 taskCounter++;
                 addTaskName(newTask, taskValue);
                 addButtons(newTask);
-                document.getElementById("tasks").appendChild(newTask);
+                document.getElementById("tasks").appendChild(newTask);            
+            }
+        }
+    }
+    if (typeof (Storage) !== "undefined") {
+        if (localStorage.getItem("todos") !== null) {
+            let todos = JSON.parse(localStorage.getItem("todos"));
+            for (let i = 0; i < todos.length; i++) {
+                let taskValue = todos[i];
+                let newTask = document.createElement("li");
+                newTask.setAttribute("id", "task-" + taskCounter);
+                taskCounter++;
+                addTaskName(newTask, taskValue);
+                addDelete(newTask);
+                document.getElementById("tasksDone").appendChild(newTask);
             }
         }
     }

48e5f4e2383d6c0c9534ff20478311687d371adc
efp2007 <efp2007@form.efp.local>
2023-03-13 10:49:10 +0100

    1.2 Retourner le nom de l'étudiant avec la meilleure note : en cours

--- examen_lpw/script.js
+++ examen_lpw/script.js
@@ -1,4 +1,32 @@
 /* 1.1 One-liner */
 let n = [18, 35, 82, 12, 51, 94, 35, 21, 11, 44, 19, 6, 70, 54, 7, 48, 16, 22, 23];
-let answer = n.filter(num => num%2 == 0).reduce((a, b) => { return a + b }, 0);
+let answer = n.filter(num => num % 2 == 0).reduce((a, b) => { return a + b }, 0);
 console.log(answer);
+
+/*1.2 Retourner le nom de l'étudiant avec la meilleur note */
+let students = [
+    { matricule: "001", nom: "Zoltar", age: 21, notes: [12, 15, 18] },
+    { matricule: "002", nom: "Balthazar", age: 22, notes: [10, 13, 16] },
+    { matricule: "003", nom: "Durant", age: 20, notes: [14, 14, 14] },
+    { matricule: "004", nom: "Razzle", age: 23, notes: [17, 12, 19] },
+    { matricule: "005", nom: "Rousseau", age: 19, notes: [9, 11, 13] },
+    { matricule: "006", nom: "Fournier", age: 21, notes: [15, 16, 14] },
+    { matricule: "007", nom: "Girard", age: 22, notes: [12, 18, 13] },
+    { matricule: "008", nom: "Zizek", age: 20, notes: [16, 11, 15] },
+    { matricule: "009", nom: "Moreau", age: 23, notes: [13, 12, 14] },
+    { matricule: "010", nom: "Bibbidi", age: 19, notes: [11, 10, 9] }
+];
+
+let best = bestOfTheBest(students);
+
+function bestOfTheBest(students) {
+    for (let i = 0; i < students.length; ++i) {
+        for (let j = 0; j < students[i].notes.length; ++j) {
+
+            console.log(students[i].notes[j]);
+
+        }
+
+    }
+
+}
